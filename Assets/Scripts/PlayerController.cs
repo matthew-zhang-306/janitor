361445d0eca273cbb3641ba399a75b09d67850e2
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2021-08-29 17:34:55 -0500

    basic player movement added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float acceleration = 1f;
+    [SerializeField] private float maxSpeed = 1f;
+
+    private Rigidbody2D rb2d;
+
+    private void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        // find the direction that the player is moving
+        Vector2 moveInput = Vector2.zero;
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput = moveInput.normalized;
+
+        // apply this velocity to the player
+        Vector2 velocity = rb2d.velocity;
+        velocity = Vector2.MoveTowards(velocity, moveInput * maxSpeed, acceleration);
+        rb2d.velocity = velocity;
+    }
+}
