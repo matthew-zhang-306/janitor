6cc16927813e26b29c947368cf061ab77acadded
thebunanna <thebunanna@gmail.com>
2021-08-30 15:07:40 -0500

    sponge now changes tilemap also includes tilemap

--- Assets/Scripts/WeaponSystem/Projectile/BaseProjectile.cs
+++ Assets/Scripts/WeaponSystem/Projectile/BaseProjectile.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BaseProjectile : MonoBehaviour
 {
     public float lifetime = 3f;
     private float m_time = 0f;
+    private Tilemap tm;
+    public Tile tile;
     // Start is called before the first frame update
     void Start()
     {
         m_time = 0f;
+        tm = GameObject.Find("/Grid").transform.GetChild(0).GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -21,7 +25,11 @@ public class BaseProjectile : MonoBehaviour
             Destroy(gameObject);
         }
         else {
-            
+            Vector3Int pos = tm.WorldToCell(this.transform.position);
+            TileBase c_tile = tm.GetTile(pos);
+            if (c_tile != null) {
+                tm.SetTile(pos, tile);
+            }
         }
     }
 
