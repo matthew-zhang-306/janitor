4c410e92680b623a25a65e5e86a3323559379b19
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2021-09-03 18:43:02 -0500

    the player collides with walls properly

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private float range = 1f;
+
+    private Camera cam;
+
+    private void Start() {
+        // there's a better way to connect this once we have a level manager
+        cam = Camera.main;
+    }
+
+    private void FixedUpdate() {
+        Vector2 mouseViewportPoint = cam.ScreenToViewportPoint(Input.mousePosition);
+        mouseViewportPoint = new Vector2(Mathf.Clamp01(mouseViewportPoint.x), Mathf.Clamp01(mouseViewportPoint.y));
+        transform.position = transform.parent.position + (2 * mouseViewportPoint - Vector2.one).ToVector3() * range;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(range, range, 1f));
+    }
+}
