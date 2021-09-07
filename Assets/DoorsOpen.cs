7161ebeddb2dd74322ca4e70dd3d3398f91f0312
WarrenPV <81312799+WarrenPV@users.noreply.github.com>
2021-09-07 14:24:57 -0500

    Really Bad TestRoom Spawn and Door close

---
+++ Assets/DoorsOpen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class DoorsOpen : MonoBehaviour
+{
+
+    
+    public TilemapRenderer doorsR;
+    public TilemapCollider2D doorsC;
+    public GameObject testroom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        
+        
+        doorsC.enabled = false;
+        doorsR.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            
+            testroom.SetActive(true);
+            doorsC.enabled = true;
+            doorsR.enabled = true;
+        }
+    }
+}
