32c425c074c0e3d5e8c78cbdf237f582baac85d1
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2021-09-08 16:47:46 -0500

    add emergency reset (press r) to school

---
+++ Assets/LevelReset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelReset : MonoBehaviour
+{
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log("reset");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(
+                UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex
+            );
+        }
+    }
+}
