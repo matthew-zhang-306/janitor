750bda68d852721c75d24561d6c2c65d813efc0f
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2021-09-07 20:55:05 -0500

    song player, extremely quick and dirty implementation

---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu()]
+public class Song : ScriptableObject
+{
+    public AudioClip song;
+    public float songStartTime;
+    
+    [Header("Leave these values at 0 to have the song not loop")]
+    public float loopStartTime;
+    public float loopEndTime;
+
+    [Header("Will be used for transitions between songs")]
+    public float measuresPerMinute;
+}
