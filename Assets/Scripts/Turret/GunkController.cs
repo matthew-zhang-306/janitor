7fa48df36968f2617e88d71c30ae28cc5b610ac1
WarrenPV <81312799+WarrenPV@users.noreply.github.com>
2021-09-17 00:30:17 -0500

    Turrets broken again

---
+++ Assets/Scripts/Turret/GunkController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunkController : MonoBehaviour
+{
+    Vector3 start_Position;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Vector3 start_position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //Destroy(this.gameObject);
+            HideGunk();
+        }
+        if (other.tag == "Wall")
+        {
+            HideGunk();
+        }
+    }
+
+    public void HideGunk()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ResetGunk()
+    {
+        gameObject.SetActive(true);
+        transform.position = start_Position;
+        
+    }
+}
--- Assets/Scripts/Turret/Turret.cs
+++ Assets/Scripts/Turret/Turret.cs
@@ -13,28 +13,48 @@ public class Turret : MonoBehaviour
 
     public float cooldown = 1f;
 
-    
+    private bool PlayerRadius;
+
     //private Animation turret_shoots;
     private Animator turret;
+
+    private GunkController gunkcontroller;
+
+    bool cooldownShoot;
     // Start is called before the first frame update
     void Start()
     {
-        //turret_shoots = gameObject.GetComponent<Animation>();
         turret = gameObject.GetComponent<Animator>();
+        turret.SetTrigger("Play");
+        GameObject go = (GameObject)Instantiate(gunk, (Vector2)transform.position + Offset * transform.localScale.x, gunk.transform.rotation);
+        go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * transform.localScale.x, velocity.y);
+        //turret_shoots = gameObject.GetComponent<Animation>();
+        
         //turret_shoots.Play();
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            canShoot = true;
+        }
+        else
+        {
+            canShoot = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (!PauseMenu.GamePaused)
         {
             
-                if (canShoot == true)
+                if (cooldownShoot == true && canShoot == true)
                 {
-                    
-                    GameObject go = (GameObject)Instantiate(gunk, (Vector2)transform.position + Offset * transform.localScale.x, gunk.transform.rotation);
-                    go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * transform.localScale.x, velocity.y);
+                //  GameObject go = (GameObject)Instantiate(gunk, (Vector2)transform.position + Offset * transform.localScale.x, gunk.transform.rotation);
+                // go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * transform.localScale.x, velocity.y);
+
+                    gunkcontroller.ResetGunk();
                     turret.SetTrigger("Play");
                     StartCoroutine("Cooldown");
 
@@ -42,19 +62,16 @@ public class Turret : MonoBehaviour
 
             }
             
-
         }
     }
 
+    
     IEnumerator Cooldown()
     {
         
-        canShoot = false;
-        yield return new WaitForSeconds(cooldown + Random.Range(1.0f, 3.0f));
-        
-        
-
-        canShoot = true;
+        cooldownShoot = false;
+        yield return new WaitForSeconds(cooldown + Random.Range(.50f, 2.0f));
+        cooldownShoot = true;
     }
 
     
