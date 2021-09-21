59775c30cd666c920dd72241472b13a9f2c46ede
WarrenPV <81312799+WarrenPV@users.noreply.github.com>
2021-09-21 09:47:12 -0500

    LevelLoader and Loading Screen

---
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    public void LoadLevel (int sceneIndex)
+    {
+        //AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+
+        loadingScreen.SetActive(true);
+
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+}
