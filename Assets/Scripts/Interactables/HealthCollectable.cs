604ef62b5271394c991dead1e8cdaa16fbf45527
thebunanna <thebunanna@gmail.com>
2021-09-29 00:01:57 -0500

    Interactables UI and explosion linetrace bug

--- Assets/Scripts/Interactables/HealthCollectable.cs
+++ Assets/Scripts/Interactables/HealthCollectable.cs
@@ -6,7 +6,10 @@ public class HealthCollectable : Interactable
 {
     public int healthAmount = 10;
     // Start is called before the first frame update
-
+    public override string ToolTip
+    {
+        get => _tooltip + "(" + healthAmount + ")";
+    }
     public override void DoAction (PlayerController pc, Inventory _)
     {
         var health = pc.GetComponent<Health>();
