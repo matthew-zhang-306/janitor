4f40c7515f2f89ca508114f859700755207553c9
WarrenPV <81312799+WarrenPV@users.noreply.github.com>
2021-10-06 12:34:46 -0500

    Lighting Scene onboarding
    wasd

---
+++ Assets/Scripts/UI/DashTutorial.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashTutorial : MonoBehaviour
+{
+    public GameObject DashUI;
+    bool playerInTrigger;
+    bool canflicker;
+
+    private void FixedUpdate()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player entered my no on square");
+            StartCoroutine(PlayerDelay());
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player still in my no no square.");
+            playerInTrigger = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerInTrigger = false;
+        canflicker = false;
+        DashUI.SetActive(false);
+    }
+
+    IEnumerator Flicker()
+    {
+        
+        while (canflicker == true)
+        {
+            DashUI.SetActive(true);
+            yield return new WaitForSeconds(2f);
+            DashUI.SetActive(false);
+            yield return new WaitForSeconds(.5f);
+            DashUI.SetActive(true);
+        }
+        DashUI.SetActive(false);
+
+
+    }
+
+    IEnumerator PlayerDelay()
+    {
+        yield return new WaitForSeconds(5f);
+        if (playerInTrigger == true)
+        {
+            canflicker = true;
+            StartCoroutine(Flicker());
+        }
+    }
+}
---
+++ Assets/Scripts/UI/WASDTutorial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDTutorial : MonoBehaviour
+{
+    public GameObject WASDUI;
+    bool playerInTrigger;
+    
+
+    private void Start()
+    {
+        
+        StartCoroutine(PlayerDelay());
+        WASDUI.SetActive(true);
+    }
+
+
+    
+
+    IEnumerator PlayerDelay()
+    {
+        
+        yield return new WaitForSeconds(5f);
+        WASDUI.SetActive(false);
+        
+        
+    }
+}
