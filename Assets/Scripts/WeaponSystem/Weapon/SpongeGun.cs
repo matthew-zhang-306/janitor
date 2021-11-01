b47ab15b924fb81e1340c7fbbc2372ebed7de6b2
Matthew Zhang <matthew.zhang.306@gmail.com>
2021-11-01 13:39:44 -0500

    add aim line for the sponge gun

--- Assets/Scripts/WeaponSystem/Weapon/SpongeGun.cs
+++ Assets/Scripts/WeaponSystem/Weapon/SpongeGun.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class SpongeGun : BaseWeapon
 {
-    private Animator gunAnimation;
-    protected override void Start()
-    {
-        base.Start();
-        gunAnimation = transform.GetChild(0).GetComponent<Animator>();
-    }
+    public Animator muzzleAnimation;
+
     public override float HandleFire(Vector3 dir, Quaternion rotation)
     {
         GameObject go;
         base.HandleFire(dir, rotation, out go);
         
         go.GetComponent<BaseProjectile>().hurtbox.gameObject.GetComponent<Damage>().damage = (int) this.bulletDamage;
-        gunAnimation.SetTrigger("active");
+        muzzleAnimation.SetTrigger("active");
         return ammoDrain;
     }
 }
---
+++ Assets/Scripts/WeaponSystem/Weapon/SpongeGunAimLine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpongeGunAimLine : MonoBehaviour
+{
+    public float spongeWidth;
+    public float LineLength { get; private set; }
+
+    private LineRenderer lineRenderer;
+    public WeaponSystem weaponSystem;
+
+
+    private void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update() {
+        lineRenderer.SetPosition(1, Vector3.right * LineLength);
+
+        if (weaponSystem != null) {
+            lineRenderer.enabled = weaponSystem.weapon.CanFire(weaponSystem._ammo);
+        }
+    }
+
+    private void FixedUpdate() {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            transform.position,
+            Vector2.one * spongeWidth,
+            transform.rotation.eulerAngles.z,
+            transform.right,
+            50f,
+            LayerMask.GetMask("Wall", "Sides")
+        );
+
+        LineLength = hit.collider != null ? hit.distance : 50f;
+    }
+}
