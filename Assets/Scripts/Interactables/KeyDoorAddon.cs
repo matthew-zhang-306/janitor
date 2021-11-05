d7321f24e10678d2117a78d68f90f40c16f38840
Matthew Zhang <matthew.zhang.306@gmail.com>
2021-11-05 11:23:25 -0500

    create key door that you can open by interacting with it, which will later require a key

---
+++ Assets/Scripts/Interactables/KeyDoorAddon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class KeyDoorAddon : Interactable
+{
+    [SerializeField] private SpriteRenderer lockedOverlay = default;
+    bool isOpen = false;
+
+    private void Start() {
+        GetComponent<Door>().CloseDoor();
+
+        // locked overlay bobbing
+        lockedOverlay.transform.DOLocalMoveY(
+            lockedOverlay.transform.localPosition.y - 0.2f, 1f
+        ).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void Update() {
+        // locked overlay flickering
+        lockedOverlay.enabled = Random.Range(0f, 1f) < 0.95f;
+    }
+
+    public override void DoAction (PlayerController pc, Inventory i)
+    {
+        if (isOpen)
+            return;
+        isOpen = true;
+        _tooltip = "";
+
+        GetComponent<Door>().OpenDoor();
+        lockedOverlay.DOFade(0, 0.5f);
+    }
+}
