219b3dd88986323dc149f4ccf60807ca0a28f27f
thebunanna <thebunanna@gmail.com>
2021-11-07 19:37:43 -0600

    Well move bindings kinda works?

--- Assets/Scripts/UI/BindingButton.cs
+++ Assets/Scripts/UI/BindingButton.cs
@@ -28,31 +28,55 @@ public class BindingButton : MonoBehaviour
 
     public void ReBind()
     {
+        //Buffer so you don't accidentally spam binding operations
         if (isBinding) return;
         isBinding = true;
 
-        var rebind = new InputActionRebindingExtensions.RebindingOperation()
-            .WithAction(bindingAction)
-            .WithCancelingThrough("<Keyboard>/escape");
+        //Begin rebinding
+        // if (bindingAction)
+        // var rebind = new InputActionRebindingExtensions.RebindingOperation()
+        //     .WithAction(bindingAction)
+        //     .WithCancelingThrough("<Keyboard>/escape")
+        //     .WithTargetBinding(index);
 
+        var rebind = bindingAction.PerformInteractiveRebinding()
+            .WithCancelingThrough("<Keyboard>/escape")
+            .WithControlsExcluding("Mouse/delta")
+            .WithControlsExcluding("Mouse/position")
+            .WithExpectedControlType("Button")
+            .WithTargetBinding(index);
         rebind.Start();
 
+        //For composite bindings (move for now)
+        
         keybind.text = String.Format("{0} : {1}", bindingAction.name, "PRESS ANY");
+        
+        
         rebind.OnComplete(ctx => {
-                Debug.Log("Rebind Complete");
-                SetString();
-                this.Invoke(()=>isBinding = false, 0.5f);
-                ctx.Dispose();
-            });
+            Debug.Log("Rebind Complete");
+            SetString();
+            this.Invoke(()=>isBinding = false, 0.5f);
+            ctx.Dispose();
+        });
+        rebind.OnCancel(ctx => {
+            Debug.Log("Rebind Canceled");
+            SetString();
+            isBinding = false;
+            ctx.Dispose();
+        });
     }
 
     public void SetString()
     {
-        keybind.text = String.Format("{0} : {1}", bindingAction.name, bindingAction.bindings[0].effectivePath ?? "None");
+        keybind.text = String.Format("{0} : {1}", bindingAction.name, 
+            bindingAction.GetBindingDisplayString(index, 
+            //InputBinding.DisplayStringOptions.DontOmitDevice | 
+            InputBinding.DisplayStringOptions.DontIncludeInteractions
+            ) ?? "None");
     }
 
     public void Clear()
     {
-        bindingAction.ChangeBinding(0).Erase();
+        bindingAction.ChangeBinding(index).Erase();
     }
 }
