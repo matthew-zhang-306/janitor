f5c8e4558dcf3e183d9eed77ae54a11993170ba6
thebunanna <thebunanna@gmail.com>
2021-11-08 21:38:47 -0600

    clean bar

--- Assets/Scripts/UI/LevelCleanBar.cs
+++ Assets/Scripts/UI/LevelCleanBar.cs
@@ -2,20 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Globalization;
 
 public class LevelCleanBar : MonoBehaviour
 {
-    public FloorController Floor;
+    public FloorController floor;
     public Slider cleanBar;
 
     // this value is currently set in the inspector
     // but it should be equal to whatever threshold the room manager has
     public float cleanThreshold;
 
+    public Transform flagParent;
+    public Image flagPrefab;
+    [SerializeField] private TextMeshProUGUI main;
+
+    private static NumberFormatInfo nfi = new CultureInfo( "en-US", false ).NumberFormat;
+
+
+    void Awake ()
+    {
+        nfi.PercentDecimalDigits = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        cleanBar.value = Mathf.Clamp01(Floor.GetCleanPercent());
+        cleanBar.value = Mathf.Clamp01(floor.GetCleanPercent());
+
+        main.text = cleanBar.value.ToString("P", nfi);
     }
+
+    public void AddGoal(float thresh) 
+    {
+        if (thresh > 1 || thresh < 0) {
+            Debug.Log("invalid wave thresh");
+            return;
+        }
+        var flag = Instantiate(flagPrefab, flagParent);
+        flag.color = Color.cyan;
+        flag.rectTransform.anchoredPosition += new Vector2 (0, thresh * 36);
+    }
+
+    public void AddWave(float thresh)
+    {
+        if (thresh > 1 || thresh < 0) {
+            Debug.Log("invalid wave thresh");
+            return;
+        }
+
+        var flag = Instantiate(flagPrefab, flagParent);
+        flag.color = Color.red;
+        flag.rectTransform.anchoredPosition += new Vector2 (0, thresh * 36);
+    }
+    public void Clear ()
+    {
+        //Clear previous wave (use for when resetting)
+        foreach (Transform flags in flagParent) {
+            Destroy(flags);
+        }
+    }
+
 }
