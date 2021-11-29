aa70787a482e326f3bc09bcbb2f961da4349ed0d
Matthew Zhang <matthew.zhang.306@gmail.com>
2021-11-28 23:16:17 -0600

    sabotage

---
+++ Assets/TrailerCameraPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerCameraPos : MonoBehaviour
+{
+    public float moveDelay;
+    public Vector3 moveDir;
+    public float moveSpeed;
+
+    private float timer;
+
+    private void FixedUpdate() {
+        timer += Time.deltaTime;
+
+        if (timer > moveDelay) {
+            transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/TrailerTitleAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TrailerTitleAnim : MonoBehaviour
+{
+    public Image flash;
+    public Image grimeSpree;
+    public RectTransform text;
+
+    private void Start() {
+        text.gameObject.SetActive(false);
+
+        Helpers.Invoke(this, () => {
+            flash.color = Color.white;
+            flash.DOFade(0, 0.8f).SetEase(Ease.Linear);
+            grimeSpree.rectTransform.anchoredPosition += new Vector2(0, 120);
+            grimeSpree.rectTransform.DOAnchorPosY(grimeSpree.rectTransform.anchoredPosition.y - 120, 2f).SetEase(Ease.OutBack);
+        }, 1f);
+
+        Helpers.Invoke(this, () => {
+            text.gameObject.SetActive(true);
+            grimeSpree.gameObject.SetActive(false);
+        }, 6f);
+    }
+}
